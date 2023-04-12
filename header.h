57f49b714bea6535da95ea6d34206d22c2555047
aruya <116321705+aruya2707@users.noreply.github.com>
2023-04-13 00:53:50 +0700

    Update header.h
    Perubahan Format

--- header.h
+++ header.h
@@ -1,35 +1,56 @@
 #ifndef Header_H
 #define Header_H
-#include "boolean.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <conio.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <limits.h>
+#include "boolean.h"
+#define First(L) (L).First
+#define NmPmbli(P) (P)->NmPmbli
+#define Barang(P) (P)->barang
+#define Next(P) (P)->next
+#define NmBrng(P) (P)->NmBrng
+#define StokBrng(P) (P)->StokBrng
+#define HrgBrng(P) (P)->HrgBrng
 
+/*Deklarasi Kamus data*/
 typedef char* infochar;
 typedef int infotype;
-typedef struct Costumer *address;
-typedef struct BrgBelian *addressbrng;
+typedef struct Customer *address;	//node2 di queue
+typedef struct BrgBelian *addressbrg;	//node yg ditunjuk queue sebagai barang belian
+typedef struct ListBarang *addresslist;
 
-typedef struct kasir{
-    int kasir_1, kasir_2, kasir_3;
-}
+//infochar Kasir[3] = {"kasir1", "kasir2","kasir3"};	//array kasir
+typedef struct{
+	infotype kasir_1,kasir_2,kasir_3;
+}kasir;
 
-typedef struct Costumer{
-    addressbrg BrngBelian;
-    infochar nmPmbli;
-    address next;
-}
+typedef struct Customer{
+	addressbrg barang;	//menunjuk ke node BrgBelian
+	infochar NmPmbli;	//nama pembeli
+//	bool membawaTroli;	//untuk troli atau keranjang
+	address next;	//pointer ke node pelanggan di depannya
+}Customer;
 
-typedef struct tListBarang{
-    infochar NmBrng;
-    infotype StokBrg;
-    intotype HrgBrng;
-}
+/*Node yang berisi list2 barang dagangan ada stok dan harga
+dimana setiap pembelianstok akan berkurang dan data pilihan
+dimasukkan ke node BrgBelian */
+typedef struct ListBarang{
+	infochar NmBrng;	
+	infotype StokBrng,HrgBrng;
+}ListBarang;
 
 typedef struct BrgBelian{
-    infochar NmBarang;
-    infotype HrgBarang;
-    infotype JmlBarang;
-    addressbrng NextBrng;
-}
+	infochar NmBrg;
+	infotype HrgBrg, JmlBrg;
+	addressbrg NextBrg;
+}BrgBelian;
 
+//pointer ke Queue
+typedef struct {
+	address First;
+}List;
+
+#endif
