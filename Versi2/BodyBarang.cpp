eb8aa42a90557b71f3b1556270d453a97df52dd4
lindasantika08 <123919343+lindasantika08@users.noreply.github.com>
2023-05-03 22:03:33 +0700

    ini barang buat di kasir(add, del)

--- Versi2/BodyBarang.cpp
+++ Versi2/BodyBarang.cpp
@@ -1,147 +1,165 @@
-#include "cassier.h"
+#include <conio.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef char* infochar;
+typedef int infotype;
 
-int ListEmpty(ListBarang Brng){
-	int c = 0;
-	FILE *listBarangnya;
-	
-	listBarangnya = fopen("daftarbarang.dat", "rb");
-	while (fread(&Brng, sizeof(Brng), 1, listBarangnya))
-	c = 1;
-	fclose(listBarangnya);
-	return c;
+typedef struct ListBarang{
+	infochar NmBrng[20];	
+	infotype StokBrng, HrgBrng, no;
+}ListBrng[100];
+
+int ReadProduct();
+
+void AddBarang(int b);
+
+void DeleteProduct(int b);
+
+int main() {
+
+    int n = 0, infal, pilih;
+    do {
+        menu:
+        infal = 0;
+        n = ReadProduct(); 
+        printf("\nPilihan : ");
+        printf("\n1.Tambah Produk\n2.Hapus Produk\n0.Keluar");
+        printf("\nPilih : ");
+        scanf("%d", &pilih); 
+        switch (pilih) {
+            case 1 : { 
+                AddBarang(n);
+                goto menu;
+                break;
+            }
+            case 2 : { 
+                DeleteProduct(n);
+                goto menu;
+                break;
+            }
+//            case 3 : {
+//            	goto menu;
+//				break;
+//			}
+        }
+    } while (pilih != 0);
+
+    return 0;
 }
 
-int readProduct(){
-	FILE *filebarang;
-	int a, ch;
-	a = 0;
-	char *list = "..\\TUBESSDAsms2\\list_barang.txt";
-	ListBarang brg;
+
+int ReadProduct(){
+	FILE *file;
+	int a=0;
+	int ch;
+	char *lis = "list_barang.txt";
+	ListBrng brg;
 	
-	if ( (filebarang = fopen(list,"r")) == NULL )
+//	file = fopen("list_barang.txt", "r");
+	// 
+	if ( (file = fopen(lis,"r"))== NULL )
 	{
 		printf("File tidak dapat dibuka!\r\n");
 	}
 	
-	system("cls");
-	printf("\t\t=========================================\n");
-	printf("\n");
-	
-	//tampil produk	
-	printf("\t\tNo\tNama Produk\t\tHarga\tJumlah\n");
-	while (fscanf(filebarang,"%d;%[^;];%d;%d\n", &brg[a].no, &brg[a].NmBrng, &brg[a].StokBrng, &brg[a].HrgBrng) != EOF)
+/*Menampilkan List barang*/
+	printf("---------------------------------------------\n");
+	printf("No\tNama Produk\t\tHarga\tStok\n");
+	printf("---------------------------------------------\n");
+	while (fscanf(file,"%d;%[^;];%d;%d\n", &brg[a].no, &brg[a].NmBrng, &brg[a].HrgBrng, &brg[a].StokBrng) != EOF)
 	{
-		printf("\t\t%d\t%-20s\t%d\t%d\n", brg[a].no, brg[a].NmBrng, brg[a].StokBrng, brg[a].HrgBrng);
+		printf("%d\t%-20s\t%d\t%d\n", brg[a].no, brg[a].NmBrng, brg[a].HrgBrng, brg[a].StokBrng);
 		a++;
 	}	
 	fclose(file);
 	
-	printf("\n\t\tjumlah baris : %d",a);
-	printf("\n\t\t-----------------------------------------\n");
-	printf("\t\t                  Menu\n");
-	printf("\t\t-----------------------------------------\n");
-	printf("\t\t1.Tambah Produk\n\t\t2.Ubah Produk\n\t\t3.Hapus Produk\n\t\t0.Keluar\n");
-	printf("\t\tPilih : ");	
-	
+	printf("\njumlah jenis produk : %d",a);	
 	return a;
 }
 
 void AddBarang(int b){
-	FILE *filebarang;
-	char *list = "..\\TUBESSDAsms2\\list_barang.txt";
-	ListBarang brg;
+	FILE *file;
+	char *dir = "list_barang.txt";
+	ListBrng brg;
 
-	if ( (file = fopen(list,"a")) == NULL )
+	if ( (file = fopen(dir,"a")) == NULL )
 	{
 		printf("File tidak dapat dibuka!\r\n");
 		exit(1);
 	}
 		
 	system("cls");
-	printf("\t\t=========================================\n");
 	printf("\n");
 		
 	brg[b].no = b;
-	printf("\t\tNama Produk\t: "); 
+	printf("Nama Produk\t: "); 
 	fflush(stdin);
 	scanf("%[^\n]", &brg[b].NmBrng);
-	printf("\t\tHarga Produk\t: "); 
+	printf("Harga Produk\t: "); 
 	scanf("%d", &brg[b].HrgBrng);
-	printf("\t\tStok Produk\t: " ); 
+	printf("Stok Produk\t: " ); 
 	scanf("%d", &brg[b].StokBrng);
 		
 	fprintf(file, "\n%d;%s;%d;%d", brg[b].no, brg[b].NmBrng, brg[b].HrgBrng, brg[b].StokBrng);
 	fclose(file);
 }
 
-void deleteProduct(int b){
-	FILE *filebarang, *file1;
-	char *list = "..\\TUBESSDAsms2\\list_barang.txt";
-	ListBarang brg;
-	infotype no, k=0, l=0, j;
-	infochar konfir1, konfir2;
+void DeleteProduct(int b){
+	FILE *file, *fl;
+	char *dir = "list_barang.txt";
+	ListBrng brg;
+	int id, x=0, y=0, z;
+	char confirm, confirm2;
 	
 	ulang :
 	system("cls");
-	printf("\t\t=========================================\n");
 	printf("\n");
-	printf("\t\tMasukkan nomor produk yang akan dihapus : "); scanf("%d",&no); //input nomor
-	if (no < 0 || no > b){
+	printf("Masukkan nomor produk yang akan dihapus : "); scanf("%d",&id);
+	if (id < 0 || id > b){
 		goto ulang;
 	}
 	else{
-		filel = fopen(list,"r");
+		fl = fopen(dir,"r");
 		do{
-			fscanf(fl,"%d;%[^;];%d;%d\n", &brg[l].no, &brg[l].NmBrng, &brg[l].HrgBrng, &brg[l].StokBrng);
+			fscanf(fl,"%d;%[^;];%d;%d\n", &brg[y].no, &brg[y].NmBrng, &brg[y].HrgBrng, &brg[y].StokBrng);
 			y++;
 		}
-		while(!feof(filel));
-		fclose(filel);
+		while(!feof(fl));
+		fclose(fl);
 		
-		printf("\t\t-----------------------------------------\n");
-		printf("\t\tNo\tNama Produk\t\tHarga\tJumlah\n");
-		printf("\t\t%d\t%-20s\t%d\t%d\n", brg[no].no, brg[no].NmBrng, brg[no].HrgBrng, brg[no].StokBrng);
-		printf("\t\t-----------------------------------------\n");
-		printf("\t\tAnda yakin ingin menghapus produk (%d. %s) ? [y/t] ",brg[no].no, brg[no].NmBrng);
+		printf("-----------------------------------------\n");
+		printf("No\tNama Produk\t\tHarga\tStok\n");
+		printf("%d\t%-20s\t%d\t%d\n", brg[id].no, brg[id].NmBrng, brg[id].HrgBrng, brg[id].StokBrng);
+		printf("-----------------------------------------\n");
+		printf("Anda yakin ingin menghapus produk (%d. %s) ? y/t ",brg[id].no, brg[id].NmBrng);
 		fflush(stdin);
-		scanf("%c",&konfir1);
-		konfir2 = toupper(konfir1);
+		scanf("%c",&confirm);
+		confirm2 = toupper(confirm);
 				
-		if(konfir2 == 'Y'){
-			for (j=no; j<b; j++){
-				strcpy(brg[j].NmBrng, brg[j+1].NmBrng);
-				brg[j].HrgBrng = brg[j+1].HrgBrng;
-				brg[j].StokBrng = brg[j+1].StokBrng;
+		if(confirm2 == 'Y'){
+			for (z=id; z<b; z++){
+				brg[z].NmBrng, brg[z+1].NmBrng;
+				brg[z].HrgBrng = brg[z+1].HrgBrng;
+				brg[z].StokBrng = brg[z+1].StokBrng;
 			}
 					
 			b--;
-			file = fopen(list,"w");
+			file = fopen(dir,"w");
 			do{
-				fprintf(file, "%d;%s;%d;%d", brg[k].no, brg[k].NmBrng, brg[k].HrgBrng, brg[k].StokBrng);
-				k++;
-				if(k<b){
+				fprintf(file, "%d;%s;%d;%d", brg[x].no, brg[x].NmBrng, brg[x].HrgBrng, brg[x].StokBrng);
+				x++;
+				if(x<b){
 					fprintf(file,"\n");
 				}
 			}	
-			while(k<b);
-			fclose(filebarang);
+			while(x<b);
+			fclose(file);
 		}
 	}
-}
-
-void list_barang(){
-	char barang[255];
-	FILE *tlistbarang;
-	
-	(fkatalog = fopen("list_barang.txt","r")) == NULL;
-	
-	while(fgets(barang, sizeof(barang), tlistbarang))
-	{
-		printf("%s", barang);
-    }
-	
-	fclose(tlistbarang);
 }
