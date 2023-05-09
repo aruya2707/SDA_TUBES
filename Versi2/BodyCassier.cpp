1c471e6ffbd153f1eafb4f669bf7dfe686eabaa3
lindasantika08 <123919343+lindasantika08@users.noreply.github.com>
2023-05-09 14:33:38 +0700

    Update BodyCassier.cpp

--- Versi2/BodyCassier.cpp
+++ Versi2/BodyCassier.cpp
@@ -90,7 +90,14 @@ void MenuCustomer(Kasir K[]) {
     Queue MyQueue;
     addressbrg brg, newBrg;
     infochar nama, namab;
-    infotype pilih, jumlah, menu, idx, harga, lagi;
+    infotype pilih, jumlah, menu, idx, harga, no;
+    int a, n = 0;
+    int b;
+    ListBrng prd;
+    cstmr cst;
+    char pesan;
+    FILE *file, *file2;
+    const char *lis = "list_barang.txt";
 
     // meminta input nama pelanggan
     printf("Masukkan nama Pelanggan : ");
@@ -98,54 +105,77 @@ void MenuCustomer(Kasir K[]) {
     printf("Masukkan pilihan bawaan anda : (1.Troli/2.Keranjang)\n");
     scanf("%d", &pilih);
     InsVLastQ(&MyQueue, nama, pilih);
-
+	
+	do 
+	{
     system("cls");
     printf("\n");
-
-    /**/
-    idx = ReadProduct();
-    /*meminta inputan user*/
-    do {
-        printf("\nMasukkan nomor barang yang ingin dibeli : ");
-        scanf("%d", &menu);
-    } while (menu < 1 || menu > idx);
-
-    /*mengambil informasi dalam file*/
-    FILE *file;
-    char *lis = "list_barang.txt";
+//    /*mengambil informasi dalam file*/
+    a = 0;
     file = fopen(lis, "r");
-    do{
-    for (int i = 0; i < menu; i++) {
-        infochar nama;
-        infotype harga, stok;
-        fscanf(file, "%*d;%m[^;];%d;%d\n", &nama, &harga, &stok);
-	if (i == 0) {
-	    printf("Masukkan jumlah barang yang ingin dibeli : ");
-	    scanf("%d", &jumlah);
-	    getchar(); // membaca karakter newline yang tersisa di buffer input
-	    brg = Alokasi_Barang(nama, harga, jumlah);
-	} else {
-	    printf("Masukkan jumlah barang yang ingin dibeli : ");
-	    scanf("%d", &jumlah);
-	    getchar(); // membaca karakter newline yang tersisa di buffer input
-	    
-	    
-	    
-	    newBrg = Alokasi_Barang(nama, harga, jumlah);
-	    if (newBrg != Nil) {
-        NextBrg(brg) = newBrg;
-        brg = newBrg;
-	    }
+    printf("No\tNama Produk\t\tHarga\tJumlah\n");
+		printf("-------------------------------------------------\n");
+		do{
+			fscanf(file,"%d;%[^;];%d;%d\n", &prd[a].no, &prd[a].NmBrng, &prd[a].HrgBrng, &prd[a].StokBrng);
+			printf("%d\t%-20s\t%d\t%d\n", prd[a].no, prd[a].NmBrng, prd[a].HrgBrng, prd[a].StokBrng);
+			a++;
+		}
+		while(!feof(file));
+		printf("------------------------------------------------\n");
+		printf("\n");
+		fclose(file);
+		
+		lagi :
+		printf("Pilih no produk : ");
+		scanf("%d", &cst[n].kode);
+		no = cst[n].kode;
+			
+		if (cst[n].kode == prd[no].no){
+			//tampil nama dan harga produk
+			printf("Nama Barang \t: %s", prd[no].NmBrng);  
+			strcpy(cst[n].order, prd[no].NmBrng);
+			printf("\ntHarga Barang \t: Rp. %d", prd[no].HrgBrng);  
+			cst[n].harga = prd[no].HrgBrng;	
+			
+			//jumlah
+			printf("\nJumlah \t\t: ");		
+			scanf("%d", &cst[n].jumlah);
+			if (cst[n].jumlah > prd[no].StokBrng){
+				printf("\n\t\t\tStok tidak cukup");
+				goto lagi;
+			}else{
+				//mengurangi produk
+				prd[no].StokBrng = prd[no].StokBrng - cst[n].jumlah;
+				
+				// Proses penulisan pada file
+				file2 = fopen(lis, "w");
+
+                if (file2 == NULL) {
+                    printf("Gagal membuka file");
+                } else {
+                    for (b = 0; b < a; b++) {
+                        fprintf(file2, "%d;%s;%d;%d\n", prd[b].no, prd[b].NmBrng, prd[b].HrgBrng, prd[b].StokBrng);
+                    }
+                    fclose(file2);
+                }
+//				while(b<a);
+//				fclose(file2);
+				
+			}
+			//pesan lagi
+			printf("\n");
+			printf("\t\t\tPesan lagi [Y/T]? ");
+			fflush(stdin);
+			scanf("%c", &pesan);
+			printf("\t\t=================================================\n");
+	
+			n++;	
+		}
+		else if (cst[n].kode == 99)
+			n = 99;
+		
 	}
-        free(nama);
-    }
-    fclose(file);
-        
-    printf("\nApakah anda ingin membeli lagi? (Y/N)\n");
-    scanf("%c", &lagi);
-	}while (lagi == 'Y' || lagi == 'y');
-    
-	Kondisi_Enqueue(K);
+	while (pesan == 'Y' || pesan == 'y');
 	
 }
 void Transaksi (){	
