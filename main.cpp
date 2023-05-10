e1594c0e971f659fe98614cb8ea25060e6640b02
Adhiyarahma_ <116626432+Adhiyarahma29@users.noreply.github.com>
2023-05-10 23:31:19 +0700

    Fix Adhiya

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include"linked.h"
+
+int main(){
+	int pilih;
+	printf("#  S E L A M A T   D A T A N G   D I   A L A M A R T   #\n");
+	printf("########################################################\n");
+	printf("1. Login Gudang\n2. Pembeli\n3. Kembali\n");
+	printf("########################################################\n");
+	printf("Pilih :");
+	scanf("%d", &pilih);
+	switch (pilih){
+		case 1 :
+			Login_Admin();	//untuk update barang-barang
+			break;
+		case 2 :
+			MenuPembeli(); //untuk eksekusi Pembeli
+			break;
+		case 3 :
+			exit(1);
+			break;
+	}
+	return 0;
+}
