82fd287b8ba9d5d446d1aef7c8396d58ba58e653
lindasantika08 <123919343+lindasantika08@users.noreply.github.com>
2023-05-22 16:44:32 +0700

    Add files via upload

---
+++ BodyCassier.cpp
@@ -0,0 +1,329 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <ctype.h>
+#include <string.h>
+#include "queue.h"
+
+char *filename = "list_barang.txt";
+// Fungsi untuk memeriksa apakah username dan password sesuai dengan akun yang terdaftar
+int cek_akun(Login_Kasir *akun, int jml_akun, char *username, char *password) {
+    int i;
+	for (i = 0; i < jml_akun; i++) {
+        if (strcmp(akun[i].username, username) == 0 && strcmp(akun[i].password, password) == 0) {
+            return i; // username dan password sesuai
+        }
+    }
+    return -1; // username dan password tidak sesuai
+}
+
+void Login_Admin(){
+	int jml_akun = 3;
+	    Login_Kasir akun[jml_akun];
+	
+	    FILE *file;
+	    file = fopen("Admin.txt", "r");
+	
+	    if (file == NULL) {
+	        printf("\t\t\t\tFile tidak dapat dibuka\n");
+	        exit(1);
+	    }
+	
+	    for (int i = 0; i < jml_akun; i++) {
+	        fscanf(file, "%s %s", akun[i].username, akun[i].password);
+	    }
+	    fclose(file);
+	
+	    char username[50];
+	    char password[50];
+	
+		system ("cls");
+		printf("\t\t\t\t[MENU GUDANG]                                          \n");
+		printf("\t\t\t\t-------------------------------------------------------\n");
+		printf("\t\t\t\t||Silahkan Masukan Username dan Password Admin Gudang||\n");
+		printf("\t\t\t\t-------------------------------------------------------\n");	            
+	    printf("\t\t\t\tUsername: ");
+	    scanf("%s", username);
+	    printf("\t\t\t\tPassword: ");
+	    scanf("%s", password);
+		printf("\t\t\t\t-------------------------------------------------------\n");
+		
+	    int kasir = cek_akun(akun, jml_akun, username, password);
+	    if (kasir != -1) {
+	        printf("\t\t\t\tLog in berhasil. Selamat datang, %s.\n", akun[kasir].username);
+	        system ("cls");
+	        MenuAdmin();
+	    } else {
+	        printf("\t\t\t\tLog in gagal. Username atau password salah. (klik untuk login ulang)\n");
+	    }
+}
+
+void Transaksi (){	
+}
+
+int MenuAdmin() {
+
+    int n = 0, infal, pilih;
+    do {
+        menu:
+        infal = 0;
+        printf("\t\t\t\t[MENU GUDANG]                                          \n");
+        printf("\t\t\t\t-------------------------------------------------------\n");
+        n = ReadProduct(); 
+		printf("\n\t\t\t\t-------------------------------------------------------\n");	            
+	    printf("\t\t\t\t|| 1. Tambah Barang                                  ||\n");
+		printf("\t\t\t\t|| 2. Hapus Barang                                   ||\n");					
+		printf("\t\t\t\t|| 3. Edit Barang                                    ||\n");	
+		printf("\t\t\t\t|| 4. Kembali                                        ||\n");
+		printf("\t\t\t\t-------------------------------------------------------\n");
+        printf("\n\t\t\t\tInputkan Pilihan : ");
+        scanf("%d", &pilih); 
+        switch (pilih) {
+            case 1 : { 
+                AddBarang(n);
+                goto menu;
+                break;
+            }
+            case 2 : { 
+                DeleteProduct(n);
+                goto menu;
+                break;
+            }
+             case 3 : {
+            	UpdateBrng(n);
+            	goto menu;
+				break;
+			}
+			case 4 : {
+				system("cls");
+				main();
+				break;
+			}
+        }
+    } while (pilih != 0);
+    //system("cls);
+
+    return 0;
+}
+
+int ReadProduct(){
+	FILE *file;
+	int a=0;
+	int ch;
+	char *lis = "list_barang.txt";
+	ListBrng brg;
+	
+//	file = fopen("list_barang.txt", "r");
+	// 
+	if ( (file = fopen(lis,"r"))== NULL )
+	{
+		printf("\t\t\t\tFile tidak dapat dibuka!\r\n");
+	}
+	
+/*Menampilkan List barang*/
+//	printf("---------------------------------------------\n");
+	printf("\t\t\t\tNo\tNama Barang\t\t\tHarga\tStok\n");
+	printf("\t\t\t\t-------------------------------------------------------\n");
+	while (fscanf(file,"\t\t\t\t%d;%[^;];%d;%d\n", &brg[a].no, &brg[a].NmBrng, &brg[a].HrgBrng, &brg[a].StokBrng) != EOF)
+	{
+		printf("\t\t\t\t%d\t%-20s\t\t%d\t%d\n", brg[a].no, brg[a].NmBrng, brg[a].HrgBrng, brg[a].StokBrng);
+		a++;
+	}	
+	fclose(file);
+	
+	printf("\n\t\t\t\tjumlah jenis produk : %d",a);	
+	return a;
+}
+
+void AddBarang(int b) {
+	FILE *file;
+	char *dir = "list_barang.txt";
+	int stok;
+	ListBrng brg;
+
+	if ((file = fopen(dir, "a")) == NULL) {
+		printf("\t\t\t\tFile tidak dapat dibuka!\r\n");
+		exit(1);
+	}
+
+	system("cls");
+	printf("\n");
+
+	brg[b].no = b;
+	printf("\t\t\t\t[TAMBAH BARANG]                                        \n");
+    printf("\t\t\t\t-------------------------------------------------------\n");
+	printf("\t\t\t\tNama Produk\t: ");
+	fflush(stdin);
+	scanf("%[^\n]", &brg[b].NmBrng);
+	printf("\t\t\t\tHarga Produk\t: ");
+	scanf("%d", &brg[b].HrgBrng);	
+	do {
+		printf("\t\t\t\tStok Produk\t: ");
+		scanf("%d", &stok);
+
+		if (stok < 0) {
+			printf("\t\t\t\tInput stok tidak valid. Harap masukkan angka yang lebih besar dari atau sama dengan nol!\n");
+		}
+	} while (stok < 0);
+
+	brg[b].StokBrng = stok;
+
+	fprintf(file, "\n\t\t\t\t%d;%s;%d;%d", brg[b].no, brg[b].NmBrng, brg[b].HrgBrng, brg[b].StokBrng);
+	fclose(file);
+	system("cls");
+}
+
+void DeleteProduct(int b){
+	FILE *file, *fl;
+	char *dir = "list_barang.txt";
+	ListBrng brg;
+	int id, x=0, y=0, z;
+	char confirm, confirm2;
+	
+	ulang :
+	system("cls");
+	printf("\n");
+	printf("\t\t\t\t[HAPUS BARANG]                                         \n");
+	printf("\t\t\t\t-------------------------------------------------------\n");
+	printf("\t\t\t\tMasukkan nomor produk yang akan dihapus : "); scanf("%d",&id);
+	if (id < 0 || id > b){
+		goto ulang;
+	}
+	else{
+		fl = fopen(dir,"r");
+		do{
+			fscanf(fl,"%d;%[^;];%d;%d\n", &brg[y].no, &brg[y].NmBrng, &brg[y].HrgBrng, &brg[y].StokBrng);
+			y++;
+		}
+		while(!feof(fl));
+		fclose(fl);
+		
+		printf("\t\t\t\t-------------------------------------------------------\n");
+		printf("\t\t\t\tNo\tNama Barang\t\t\tHarga\tStok\n");
+		printf("\t\t\t\t%d\t%-20s\t\t%d\t%d\n", brg[id].no, brg[id].NmBrng, brg[id].HrgBrng, brg[id].StokBrng);
+		printf("\t\t\t\t-------------------------------------------------------\n");
+		printf("\t\t\t\tAnda yakin ingin menghapus produk (%d. %s) ? y/t ",brg[id].no, brg[id].NmBrng);
+		fflush(stdin);
+		scanf("%c",&confirm);
+		confirm2 = toupper(confirm);
+				
+		if(confirm2 == 'Y'){
+			for (z=id; z<b; z++){
+				brg[z].NmBrng, brg[z+1].NmBrng;
+				brg[z].HrgBrng = brg[z+1].HrgBrng;
+				brg[z].StokBrng = brg[z+1].StokBrng;
+			}
+			//proses penulisan ulang pada file		
+			b--;
+			file = fopen(dir,"w");
+			do{
+				fprintf(file, "%d;%s;%d;%d", brg[x].no, brg[x].NmBrng, brg[x].HrgBrng, brg[x].StokBrng);
+				x++;
+				if(x<b){
+					fprintf(file,"\n");
+				}
+			}	
+			while(x<b);
+			fclose(file);
+			system("cls");
+		}
+	}
+}
+
+void UpdateBrng(int b) {
+	FILE *file;
+	char *dir = "list_barang.txt";
+	ListBrng brg;
+	int id, d = 0, c, stok;
+
+	system("cls");
+	printf("\n");
+
+	do { // Repeat if input is invalid
+		printf("\t\t\t\t[EDIT BARANG]                                          \n");
+        printf("\t\t\t\t-------------------------------------------------------\n");
+		printf("\t\t\t\tMasukkan nomor produk yang akan diubah: ");
+		scanf("%d", &id);
+		for (c = 0; c <= id; c++) {
+			if (id == brg[c].no) {
+				printf("\t\t\t\t-------------------------------------------------------\n");
+				printf("\t\t\t\tNo\tNama Barang\t\t\tHarga\tJumlah\n");
+				printf("\t\t\t\t%d\t%-20s\t\t%d\t%d\n", brg[c].no, brg[c].NmBrng, brg[c].HrgBrng, brg[c].StokBrng);
+				printf("\t\t\t\t-------------------------------------------------------\n");
+				printf("\t\t\t\tNama Barang yang Baru\t: ");
+				fflush(stdin);
+				scanf("%[^\n]", &brg[c].NmBrng);
+				printf("\t\t\t\tHarga Barang yang Baru\t: ");
+				scanf("%d", &brg[c].HrgBrng);
+				do {
+					printf("\t\t\t\tStok Barang yang Baru\t: ");
+					scanf("%d", &stok);
+					if (stok < 0) {
+						printf("\t\t\t\tInput stok tidak valid. Silakan masukkan nilai stok yang lebih besar dari 0.\n");
+					}
+				} while (stok < 0);
+				brg[c].StokBrng = stok;
+
+				file = fopen(dir, "w");
+				system("cls");
+				do {
+					fprintf(file, "%d;%s;%d;%d", brg[d].no, brg[d].NmBrng, brg[d].HrgBrng, brg[d].StokBrng);
+					d++;
+					if (d < b) {
+						fprintf(file, "\n");
+					}
+				} while (d < b);
+				fclose(file);
+			}
+			else continue;
+		}
+	} while (id < 0 && id > b);
+}
+
+// Prosedur dari referensi internet untuk menentukan posisi 
+void gotoxy(int x, int y){
+	HANDLE hConsoleOutput;  
+	COORD dwCursorPosition;  
+	dwCursorPosition.X = x;  
+	dwCursorPosition.Y = y;  
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);  
+	SetConsoleCursorPosition(hConsoleOutput,dwCursorPosition);   
+}
+
+// Menampilkan judul kalkulator
+void tampilan(){
+int i;
+system("cls");
+i=1;
+gotoxy(5,1);printf("##================================[ S E L A M A T   D A T A N G ]=================================##");
+gotoxy(5,3);printf("##================================              D I              =================================##");
+while (i<=10){
+	gotoxy(5,4);	printf("||");																	gotoxy(110,4);	printf("||");
+	gotoxy(5,5);	printf("||");																	gotoxy(110,5);	printf("||");
+	gotoxy(5,6);	printf("||");																	gotoxy(110,6);	printf("||");
+	gotoxy(5,7);	printf("||");																	gotoxy(110,7);	printf("||");
+	gotoxy(5,8);	printf("||");																	gotoxy(110,8);	printf("||");
+	gotoxy(5,9);	printf("||");																	gotoxy(110,9);	printf("||");
+	gotoxy(5,10);	printf("||");																	gotoxy(110,10);	printf("||");
+	gotoxy(5,11);	printf("||");																	gotoxy(110,11);	printf("||");
+	
+	
+	
+	
+	
+	gotoxy(i,5);	printf("\t\t    *#*    ##         *#*    *#     #*    *#*    *######*  *######*  \n");
+	gotoxy(19-i,6);	printf("\t\t   #   #   ##        #   #   ###   ###   #   #   ##     #     ##     \n");
+	gotoxy(i-1,7);	printf("\t\t  #     #  ##       #     #  ## # # ##  #     #  #######*     ##     \n");
+	gotoxy(19-i,8);	printf("\t\t  #######  ##       #######  ##  *  ##  #######  ##*##        ##     \n");
+	gotoxy(i-1,9);	printf("\t\t  ##   ##  ##       ##   ##  ##     ##  ##   ##  ##   #       ##     \n");
+	gotoxy(19-i,10);printf("\t\t  ##   ##  #######  ##   ##  ##     ##  ##   ##  ##    #      ##     \n");
+	Sleep(300);
+	i++;
+	}
+gotoxy(5,12);printf("##=========================== S E L A M A T  B E R B E L A N J A ====================================##");
+}
+
---
+++ main.cpp
@@ -0,0 +1,293 @@
+#include"linked.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include"queue.h"
+
+datakasir A[5];
+
+main(){
+	address_P Head_Pelanggan = NULL;
+	address_K Head_Keranjang = NULL;
+	address_B Head_Barang = NULL;
+	address_P Pelanggan = NULL;
+	address_B Barang;
+	address_K Keranjang;
+	address_A Antrian;
+	int pil,pilsub1,pilsub2,nomor, lagi,kode,jml,nokasir,harga,Nomor,i=0, k;
+	infotype dompet=125000, nmr_ksr = NULL, Qty;
+	infotype2 nama,Nama_Barang,riwayat;
+	bool back_1 = false, back_mn = false, menu_pelanggan = false, menu_kasir = false, menu_admin = false;
+	int a, n=0;
+	ListBrng prd;
+	FILE *file, *fl, *fa;
+	
+	A[0].no_kasir = 1;
+	A[1].no_kasir = 2;
+	A[2].no_kasir = 3;
+	
+	char *dir = "list_barang.txt";
+	a = 0;
+		system ("cls");
+		
+		printf("\n");
+		printf("=================================================\n");
+		
+		//Menampilkan produk
+		fl = fopen(dir,"r");
+		printf("No\tNama Produk\t\tHarga\tJumlah\n");
+		printf("-------------------------------------------------\n");
+		do{
+			fscanf(fl,"%d;%[^;];%d;%d\n", &prd[a].no, &prd[a].NmBrng, &prd[a].HrgBrng, &prd[a].StokBrng);
+			printf("%d\t%-20s\t%d\t%d\n", prd[a].no, prd[a].NmBrng, prd[a].HrgBrng, prd[a].StokBrng);
+			a++;
+		}
+		while(!feof(fl));
+		printf("-------------------------------------------------\n");
+		printf("99\tKembali\n");
+		printf("=================================================\n");
+		printf("\n");
+		fclose(fl);
+	
+	do{
+		tampilan();
+		back_mn = false;
+		system("cls");
+		printf("##================================[S E L A M A T   D A T A N G   D I   A L A M A R T]=================================##");
+		printf("\n\n");
+		printf("\t\t\t\t[MENU UTAMA]                                           \n");
+		printf("\t\t\t\t-------------------------------------------------------\n");
+		printf("\t\t\t\t||                                                    ||\n");		
+		printf("\t\t\t\t||             ---PROGRAM ANTRIAN KASIR---            ||\n");
+		printf("\t\t\t\t||                                                    ||\n");
+		printf("\t\t\t\t||                  1. MENU PELANGGAN                 ||\n");
+		printf("\t\t\t\t||                  2. MENU KASIR                     ||\n");
+		printf("\t\t\t\t||                  3. MENU GUDANG                    ||\n");
+		printf("\t\t\t\t||                                                    ||\n");	
+		printf("\t\t\t\t-------------------------------------------------------\n");
+		printf("\t\t\t\tInput Pilihan:");
+		scanf("%d",&pil);
+		switch(pil){
+			case 1:
+				do{	 
+					back_1 = false;
+					system("cls");
+					printf("\t\t\t\t[MENU PELANGGAN]                                       \n");
+					printf("\t\t\t\t-------------------------------------------------------\n");
+					printf("\t\t\t\t||                                                    ||\n");
+					printf("\t\t\t\t||                 1. Pilih Pelanggan                 ||\n");
+					printf("\t\t\t\t||                 2. Tambah Pelanggan                ||\n");
+					printf("\t\t\t\t||                 3. Menu Utama                      ||\n");					
+					printf("\t\t\t\t||                                                    ||\n");	
+					printf("\t\t\t\t-------------------------------------------------------\n");
+					printf("\t\t\t\tInput Pilihan:");
+					scanf("%d", &pilsub1);
+					switch(pilsub1){
+						case 1:
+							if(IsEmpty_Pelanggan(Head_Pelanggan)){
+								printf("\t\t\t\tPelanggan Kosong\n\t\t\t\tTambahkan pelanggan terlebih dahulu\n");
+								system("pause");
+								back_1 = true;
+							}else{
+								printf("\t\t\t\tList Pelanggan : \n");
+								Pelanggan = Head_Pelanggan;
+								Tampil_List_Pelanggan(Head_Pelanggan);
+								printf("\n\t\t\t\tLihat Pelanggan : ");
+								scanf("%d",&nomor);
+								for(int i=1;i<nomor;i++){
+									Pelanggan = Pelanggan->next;
+								}
+							}
+						break;
+						case 2:
+							printf("\t\t\t\tMasukkan nama Pelanggan : ");
+							scanf("%s",&nama);
+							Enqueue_Pelanggan(&Head_Pelanggan,&Head_Keranjang,dompet,nama,nmr_ksr);
+							Pelanggan = Head_Pelanggan;
+							while(Pelanggan->next != NULL){
+								Pelanggan = Pelanggan->next;	
+							}
+						break;
+						case 3:
+							back_1 = true;
+						break;
+						default:
+						break;
+					}
+					if((pilsub1 == 1 || pilsub1 == 2 ) && ( back_1 != true )){
+						do{
+							menu_pelanggan = false;
+							system("cls");
+							printf("\t\t\t\t[TAMBAH PELANGGAN]                                     \n");
+							printf("\t\t\t\t-------------------------------------------------------\n");
+							printf("\t\t\t\t No Kasir : %d                                         \n",Pelanggan->Nmr_ksr);
+							printf("\t\t\t\t Nama     : %s                                         \n",&Pelanggan->Nm_pelanggan);
+							printf("\t\t\t\t-------------------------------------------------------\n");
+							if(!IsEmpty_Keranjang(Head_Keranjang)){
+								printf("\t\t\t\t|| 99. Masuk Antrian                                 ||\n");
+							}
+							printf("\t\t\t\t|| 1. Tambah Barang                                  ||\n");				
+							printf("\t\t\t\t|| 2. Lihat Keranjang                                ||\n");	
+							printf("\t\t\t\t|| 3. Kembali                                        ||\n");
+							printf("\t\t\t\t-------------------------------------------------------\n");
+							printf("\t\t\t\tInput Pilihan:");
+							scanf("%d",&pilsub2);
+							switch(pilsub2){
+								case 99:
+									printf("\t\t\t\tMasukan Nomor Kasir : ");
+									scanf("%d",&nomor);
+									Pelanggan->Nmr_ksr = nomor;
+									Antrian = NULL;
+									
+									Create_Node_Antrian(&Antrian);
+									Isi_Node_Antrian(&Antrian, Pelanggan->Nm_pelanggan);
+									Ins_Akhir_Antrian(&A[nomor-1].p,Antrian);
+									printf ("\n\t\t\t\tHoree!! Anda sudah masuk antrian. (klik untuk kembali)");
+									getch();
+									menu_pelanggan = true;
+									
+								break;
+								case 1:
+								    n = 0;
+								    lagi = 1;
+								    system("cls");
+								    printf("\t\t\t\t[TAMBAH PELANGGAN]                                     \n");
+									printf("\t\t\t\t-------------------------------------------------------\n");
+									printf("\t\t\t\t No Kasir : %d                                         \n",Pelanggan->Nmr_ksr);
+									printf("\t\t\t\t Nama     : %s                                         \n",&Pelanggan->Nm_pelanggan);
+									printf("\t\t\t\t-------------------------------------------------------\n");
+								    ReadProduct();  // Membaca data barang dari file ke dalam array prd
+								    printf("\n\t\t\t\t-------------------------------------------------------\n");
+								    while (lagi == 1) {
+								        printf("\n\t\t\t\tPilih Kode Barang: ");
+								        scanf("%d", &kode);
+								        // Cari indeks yang sesuai dengan nomor barang yang dipilih
+										for (n = 0; n < a; n++) {
+										    if (prd[n].no == kode) {
+										        break;
+										    }
+										}
+								        printf("\t\t\t\tMasukan Jumlah Barang: ");
+									    scanf("%d", &jml);
+									
+									    if (jml > prd[n].StokBrng) {
+									        printf("\t\t\t\tStok barang tidak mencukupi. Silakan masukkan jumlah yang valid.\n");
+									        continue; // Minta pembeli untuk menginput kembali jumlah barang
+									    }
+								        Nama_Barang = prd[n].NmBrng;
+								        harga = prd[n].HrgBrng * jml;
+								        prd[n].StokBrng = prd[n].StokBrng - jml; //proses pengurangan barang
+								
+								        // Menulis ulang semua data barang ke file
+								        FILE *file;
+								        file = fopen("list_barang.txt", "w");
+								        if (file == NULL) {
+								            printf("\t\t\t\tGagal membuka file\n");
+								            exit(1);
+								        }
+								        for (int x = 0; x < a; x++) {
+								            fprintf(file, "\t\t\t\t%d;%s;%d;%d\n", prd[x].no, prd[x].NmBrng, prd[x].HrgBrng, prd[x].StokBrng);
+								        }
+								        fclose(file);
+								
+								        if (prd[n].StokBrng < 0) {
+								            printf("\t\t\t\tMaaf Barang Habis\n");
+								            prd[n].StokBrng = 0;
+								        }
+								        Pelanggan->Keranjang = Head_Keranjang;
+								        Head_Keranjang = Pelanggan->Keranjang;
+								        Enqueue_Keranjang(&Head_Keranjang, harga, Nama_Barang, jml);
+								        printf("\n\t\t\t\tBelanja Lagi? [1.Ya, 2.Tidak] \n\t\t\t\tInput Pilihan: ");
+								        scanf("%d", &lagi);
+								    }
+								    Pelanggan->Keranjang = Head_Keranjang;
+								    Create_Node_Keranjang(&Keranjang);
+								    Isi_Node_Keranjang(&Keranjang, Barang->HrgBrng, Barang->NmBrng, Qty);
+								    Ins_Akhir_Keranjang(&Pelanggan->Keranjang, Keranjang);
+								    menu_pelanggan = false;
+								    Head_Keranjang = Pelanggan->Keranjang;
+								    printf("\n\t\t\t\t||Asik Barang belanjaan anda sudah kami masukan keranjang||\n\t\t\t\t!!!Klik lagi untuk kembali ke menu sebelumnya!!!");
+								    getch();
+
+								break;
+								case 2 :	
+									Tampil_List_Keranjang(Head_Keranjang);
+									getch();
+								break;
+								case 3 :
+									menu_pelanggan = true;
+								break;
+							}
+						}while(menu_pelanggan != true);
+					}	
+				}while(back_1 != true);
+			break;
+				
+			case 2:
+	   		 do {
+		        back_1 = false;
+		        system("cls");
+		        //menampilkan antrian 
+				for(k==0 ; k<3; k++){
+		        	printf("\t\t\t\tList Antrian : \n");
+	        	    Tampil_List_Antrian(A[k].p);
+	        	  //  getch();    
+				}
+		       	printf("\t\t\t\t[MENU KASIR]                                           \n");
+				printf("\t\t\t\t-------------------------------------------------------\n");
+				printf("\t\t\t\t||                                                    ||\n");
+				printf("\t\t\t\t||                 Pilih Kasir 1/2/3                  ||\n");					
+				printf("\t\t\t\t||                                                    ||\n");	
+				printf("\t\t\t\t-------------------------------------------------------\n");
+		        printf("\t\t\t\tInput pilihan: ");
+		        scanf("%d", &nomor);
+		
+		        if (nomor < 1 || nomor > 3) {
+		            printf("\t\t\t\tKasir tidak ada. (Klik untuk memilih ulang)\n");
+		            getch();
+		            break; // Exit the do-while loop and go back to the main menu
+	        }
+	
+	        nomor = nomor - 1;
+	        do {
+	            menu_kasir = false;
+	            system("cls");
+				printf("\t\t\t\t[MENU KASIR]                                           \n");
+				printf("\t\t\t\t-------------------------------------------------------\n");
+				printf("\t\t\t\t No Kasir : %d                                         \n",A[nomor].no_kasir);
+				printf("\t\t\t\t-------------------------------------------------------\n");	            
+				printf("\t\t\t\t|| 1. Proses Antrian Pertama                         ||\n");					
+				printf("\t\t\t\t|| 2. Kembali ke Menu Kasir                          ||\n");	
+				printf("\t\t\t\t|| 3. Kembali ke Menu Utama                          ||\n");
+				printf("\t\t\t\t-------------------------------------------------------\n");
+
+	            printf("\t\t\t\tInput Pilihan : ");
+	            scanf("%d", &pil);
+	            switch(pil) {
+	                case 1:
+	                    Del_Awal_Antrian(&A[nomor].p, &riwayat);
+	                    printf("\t\t\t\tMenghapus Antrian : %s", &riwayat);
+	                    printf("\n\t\t\t\tAntrian berhasil diproses. (klik untuk pilih antrian berikutnya)");
+	                    getch();
+	                    break;
+	                case 2:
+	                    menu_kasir = true;
+	                    break;
+	                case 3:
+	                    menu_kasir = true;
+	                    back_1 = true;
+	                    break;
+	            }
+	        } while (menu_kasir != true);
+	    } while (back_1 != true);
+	    break;
+	    
+		case 3:
+			Login_Admin();	//untuk update barang-barang
+			break;
+		}
+			
+		}while(back_mn != true);
+}
+	
+	
